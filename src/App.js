4bfbcc32e48ad59593891384fe5ee4bbad1fcd14
Ridge <ridgechristensen28@mail.com>
2022-12-20 21:41:45 -0700

    part 1 finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header"
+import MovieScreen from './components/MovieScreen';
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+
 
 function App() {
+  const [movieList, setMovieList] = useState([])
+  const [watchlist, setWatchList] = useState([])
+  const [page, setPage] = useState(1)
+
+
+  const getData = () => {
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+    .then((res) => {
+      setMovieList(res.data.results)
+      
+    })
+  }
+  useEffect(() => {
+    getData()
+  }, [page])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <main>
+        <MovieScreen 
+        list = {watchlist}
+        page = {page}
+        setPage = {setPage}
+        movieList = {movieList}/>
+      </main>
     </div>
   );
 }
 
+
+
+
+
 export default App;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = (props) => {
+    <div>
+        <header>Placeholder</header>
+        <h2>Ridge's Movie App</h2>
+    </div>
+}
+
+export default Header
